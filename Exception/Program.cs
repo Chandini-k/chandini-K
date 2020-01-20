5f487dcb060b892f5af805c123d62fcb4aba2178
Chandini-k <60084061+Chandini-k@users.noreply.github.com>
2020-01-20 16:27:32 +0530

    Add files via upload

---
+++ Exception/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine(num / 2);
+                try
+                {
+                    int[] a = new int[] { 1, 2, 3 };
+                    Console.WriteLine(a[3]);
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("inner\n"+e.Message + "\n" + e.Source + "\n" + e.StackTrace);
+                }
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message+"\n"+e.Source+"\n"+e.StackTrace);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message + "\n" + e.Source + "\n" + e.StackTrace);
+            }
+           
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n" + e.Source + "\n" + e.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("Program ended");
+            }
+            Console.ReadLine();
+        }
+    }
+}
