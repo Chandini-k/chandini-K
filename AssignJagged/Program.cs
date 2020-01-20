7e06be309c7d121f533098e5757d660b3c1bc4d0
Chandini-k <60084061+Chandini-k@users.noreply.github.com>
2020-01-20 13:28:33 +0530

    Add files via upload

---
+++ AssignJagged/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignJagged
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] a = new int[2][];
+            for(int row=0;row<2;row++)
+            {
+                for(int col=0;col<a[row].Length;col++)
+                {
+                    Console.Write(" " + a[row][col] + "\t");
+                }
+                Console.WriteLine();
+            }
+            for(int row=0;row<2;row++ )
+            {
+                for(int col=0;col<a[row].Length;col++)
+                {
+
+                }
+            }
+
+        }
+    }
+}
