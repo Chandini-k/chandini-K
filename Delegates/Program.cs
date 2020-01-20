c491ef145c26af301813cf4987d44b9eff09f9e8
Chandini-k <60084061+Chandini-k@users.noreply.github.com>
2020-01-20 16:23:04 +0530

    Add files via upload

---
+++ Delegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void deligatePointer();
+    public delegate double calareaPointer(double x);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+            Program p = new Program();
+            Calculate c = new Calculate();
+            // deligatePointer mydeligate = new deligatePointer(p.print);
+            deligatePointer mydeligate = p.print;
+            mydeligate += p.display;
+            mydeligate();
+            mydeligate -= p.display;
+            mydeligate();
+            calareaPointer cptr = Calculate.calArea;
+            double area = cptr(20);
+            Console.WriteLine(area);
+            Console.ReadLine();
+        }
+        public void print()
+        {
+            Console.WriteLine("deligate");
+        }
+        public void display()
+        {
+            Console.WriteLine("Welcome");
+        }
+    }
+}
