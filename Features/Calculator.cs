e32d8d046284f6ffa0b764ddb59cfd490d3110cb
Chandini-k <60084061+Chandini-k@users.noreply.github.com>
2020-01-20 16:30:11 +0530

    Add files via upload

---
+++ Features/Calculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    class Calculator
+    {
+        int val1;
+        int val2;
+        public int Val1 { get => val1; set => val1 = value; }
+        public int Val2 { get => val2; set => val2 = value; }
+        public Calculator()
+        {
+
+        }
+        public Calculator(int val1, int val2)
+        {
+            this.val1 = val1;
+            this.val2 = val2;
+        }
+        public int Add(int num1, int num2)
+            {
+            return (num1 + num2);
+            //Console.WriteLine("add: "+(num1 + num2));
+            }
+        public void Subtract(int num1,int num2,out int res)
+        {
+            res = num1 - num2;
+        }
+        //public void swap(int num1,int num2)//call by value
+        //{
+        //    int temp = num1;
+        //    num1 = num2;
+        //    num2 = temp;
+        //    Console.WriteLine("numbers swap");
+        //    Console.WriteLine(num1);
+        //    Console.WriteLine(num2);
+
+        //}
+        public void swap(ref int num1, ref int num2)//call by reference
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("numbers swap");
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+
+        }
+    }
+
+}
---
+++ Features/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            Console.WriteLine("enter numbers");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int result=cal.Add(x,y);
+            Console.WriteLine("add:" + result);
+            cal.Subtract(x, y, out int sub);
+            Console.WriteLine("sub:" + sub);
+            cal.swap(ref x,ref y);
+            Console.WriteLine("x value:"+x);
+            Console.WriteLine("y value:" + y);
+            Console.ReadLine();
+
+        }
+    }
+}
