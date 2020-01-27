0eeacf150bc30741c617c67e3c88c4ad3d68cfe0
Chandini-k <60084061+Chandini-k@users.noreply.github.com>
2020-01-27 09:33:52 +0530

    Add files via upload

---
+++ Thread/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Threa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadClass th = new ThreadClass();
+            Thread t = new Thread(th.countEven);
+            Thread t1 = new Thread(th.countOdd);
+            //t.Priority = ThreadPriority.Lowest;
+            //t1.Priority = ThreadPriority.Highest;
+            t.Start();
+            t1.Start();
+            //t.Suspend();
+            //Console.WriteLine("thread got suspended");
+            //t.Resume();
+            if (!t1.IsAlive)
+                t.Interrupt();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Thread/ThreadClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Threa
+{
+    class ThreadClass
+    {
+        public void countEven()
+        {
+            lock (this)
+            {
+                for (int i = 0; i < 100; i += 2)
+                {
+                    Console.WriteLine(i);
+                    //if (i == 50) Thread.Sleep(3000);
+                }
+            }
+
+        }
+        public void countOdd()
+        {
+            for (int i = 1; i < 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
