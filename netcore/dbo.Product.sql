6904eaab43b0ed8efb39647f47d656865831ea21
Chandini-k <chandini22051998@gmail.com>
2020-02-13 13:42:15 +0530

    Netcore commit

---
+++ netcore/dbo.Product.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [pid]   INT         NULL,
+    [name]  VARCHAR (5) NULL,
+    [price] INT         NULL,
+    [stock] INT         NULL,
+    PRIMARY KEY CLUSTERED ([pid] ASC)
+);
+
